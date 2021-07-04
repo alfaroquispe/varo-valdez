b33710650ce4bc3aa20917b5fa58a98037be0aff
EMiR_IDaV <alfaroquispe@gmail.com>
2021-07-04 01:31:12 -0400

    nombre .html

--- functions.js
+++ functions.js
@@ -12,16 +12,21 @@ window.onscroll = function (e) {
         element.classList.add("aqchange");
         $("#logo").attr("src", "images/logo_varo-va_neg.png");
         $("#logo").attr("width", "120");
-        var element = document.getElementById("navbarSupportedContent");
+        // var element = document.getElementById("navbarSupportedContent");
         // element.classList.remove("align-self-start");
     } else {
         var element = document.getElementById("active-border");
         element.classList.remove("aqchange");
         $("#logo").attr("src", "images/logo_varo-va_pos.png");
         $("#logo").attr("width", "145");
-        var element = document.getElementById("navbarSupportedContent");
+        // var element = document.getElementById("navbarSupportedContent");
         // element.classList.add("align-self-start");
     }
+}
+
+function aqtoggle () {
+    var element = document.getElementById("aqnavbar");
+        element.classList.toggle("aqnavbar-toggle");
 }
 
 // ------- Carousel three cards -------
